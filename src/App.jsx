2540a9cea58071e359588ae1f6e0710604387fb0
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-08-16 18:20:56 +0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-      </Routes>
-    </Router>
+    <>
+      <LandingPage />
+    </>
+
   );
 }
 
