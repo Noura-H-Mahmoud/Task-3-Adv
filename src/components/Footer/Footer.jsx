9f67c6169c68f47366a70b48bdb37cfffbbef6bf
Noura-H-Mahmoud <nouramahmoud@Nouras-MacBook-Air.local>
2024-08-16 18:30:50 +0400

    for footer commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
             <p>
                 Copyright © 2048 Villa Agency Co., Ltd. All rights reserved. Design: TemplateMo
             </p>
